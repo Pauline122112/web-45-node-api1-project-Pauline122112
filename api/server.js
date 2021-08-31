0608777b35c2ffb5bf8eb22d4fcd5bb785f66fc9
Pauline122112 <pauline.design007@gmail.com>
2021-08-30 20:31:06 -0500

    made changes

--- api/server.js
+++ api/server.js
@@ -1,11 +1,48 @@
 // BUILD YOUR SERVER HERE
+//import 
 const express = require("express");
-
+//instance of express app
 const server = express();
+//global middleware-boilerplate// teaches expr3ess to read JSON
 server.use(express.json());
 
-server.get('/hello', (req, res) => {
-    res.status(200).json({ message: "hey there" });
+//GET 
+server.get('/', (req, res) => {
+    res.status(200).json({ message: "pauline is here" });
 })
+
+//| POST   | /api/users 
+server.post('/api/users', (req, res) => {
+    res.json(
+			"creates a user using the information sent inside the `request body`."
+		);
+})
+
+// | GET    | /api/users 
+server.get("/api/users", (req, res) => {
+	res.json("Returns an array users.");
+});
+
+//| GET    | /api/users/:id 
+server.get("/api/users/:id", (req, res) => {
+	res.json("Returns the user object with the specified `id`.");
+});
+
+
+//| DELETE | /api/users/:id
+server.delete("/api/users/:id", (req, res) => {
+	res.json(
+		"Removes the user with the specified `id` and returns the deleted user."
+	);
+});
+
+
+//| PUT    | /api/users/:id
+
+server.put("/api/users/:id", (req, res) => {
+	res.json(
+		"Updates the user with the specified `id` using data from the `request body`. Returns the modified user"
+	);
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 5000
  console.log('listening on port 5000')
 // START YOUR SERVER HERE
 
+server.use('*', (req, res) => {
+    res.status(404).json({ message: 'sorry, not found!'})
+})
+
 server.listen(port, () => {
     console.log(`listening on, ${port}`)
 })
